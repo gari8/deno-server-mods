4eb1fac9bebab3e8eb25505939659bc862dcf6c1
HAGARIHAYATO <hagari.hayato.infratop@gmail.com>
2020-12-20 17:58:50 +0900

    init

---
+++ mod.ts
@@ -0,0 +1,43 @@
+// @ts-ignore
+import { listenAndServe, ServerRequest, Response } from "https://deno.land/std@0.81.0/http/server.ts";
+
+export class ServerMod {
+
+    private _req: ServerRequest
+
+    constructor(req?: ServerRequest) {
+        this._req = req!;
+    }
+
+    GET = (url: string, f: (req?:ServerRequest) => Promise<void>) => {
+        if (this._req.method === "GET" && this._req.url === url) {
+            console.log("method: " + this._req.method)
+            // @ts-ignore
+            f(this._req)
+        }
+    }
+
+    POST = (url: string, f: (req?:ServerRequest) => Promise<void>) => {
+        if (this._req.method === "POST" && this._req.url === url) {
+            console.log("method: " + this._req.method)
+            // @ts-ignore
+            f(this._req)
+        }
+    }
+
+    PUT = (url: string, f: (req?:ServerRequest) => Promise<void>) => {
+        if (this._req.method === "PUT" && this._req.url === url) {
+            console.log("method: " + this._req.method)
+            // @ts-ignore
+            f(this._req)
+        }
+    }
+
+    DELETE = (url: string, f: (req?:ServerRequest) => Promise<void>) => {
+        if (this._req.method === "DELETE" && this._req.url === url) {
+            console.log("method: " + this._req.method)
+            // @ts-ignore
+            f(this._req)
+        }
+    }
+}
